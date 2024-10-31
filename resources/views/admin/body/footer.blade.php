cc082968b7a8bdda989312cc53db5409b99c5fc1
Gabriel <gafwebmaster@gmail.com>
2024-10-31 10:05:58 +0200

    Wip

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Gabriel Agiurgioaie
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Laravel 10 <i class="mdi mdi-heart text-danger"></i> Web Developer
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
